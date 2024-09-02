24e94eedff0c0927cced76aa8dddeeea6f9d10d4
Harinithya <harinithyaveerla@gmail.com>
2024-09-02 11:23:40 +0530

    Added to side navigation bar to profile page

--- app/profile_page.py
+++ app/profile_page.py
@@ -1,5 +1,6 @@
 import mesop as me
 from app import global_state
+from components.side_navigation_bar import side_navigation
 
 def profile():
     if not global_state.logged_status:
@@ -37,91 +38,101 @@ def profile():
                     me.button("Back to Home", type="raised", on_click=lambda event: me.navigate("/"),
                               style=me.Style(background="radial-gradient(#0B0C10, #1F2833)", color="white"))
     else:
-        # TO-DO : ADD SIDENAV UI
-
         # Background box styling
-        with me.box(style=me.Style(padding=me.Padding.all(0),
-                                   width=me.viewport_size().width,
-                                   height=me.viewport_size().height,
-                                   background="content-box radial-gradient(#0B0C10, #1F2833)",
-                                   display="flex",
-                                   justify_content="center",
-                                   align_items="center",
-                                   flex_direction="column", )
+        with me.box(style=me.Style(
+                    background="content-box linear-gradient(#0B0C10,#1F2833)",
+                    width=me.viewport_size().width - 8,
+                    height=me.viewport_size().height - 4,
+                )
                     ):
+            side_navigation()
             # Profile picture
-            with me.box(style=me.Style(margin=me.Margin.all(0),
-                                       background="black",
-                                       width=500,
-                                       height=550,
-                                       display="flex",
-                                       justify_self="center",
-                                       # justify_content="center",
-                                       align_items="center",
-                                       border_radius=50,
-                                       box_shadow="4px 4px 4px grey",
-                                       flex_direction="column",
-                                       gap=5,
-                                       )
-                        ):
-                me.icon("person", style=me.Style(
-                    margin=me.Margin.symmetric(vertical=65),
-                    color="black",
-                    background="white",
-                    border_radius=25,
-                    width=40,
-                    height=40,
+            with me.box(style=me.Style(
+                    # background="content-box radial-gradient(#0B0C10, #1F2833)",
+                    padding=me.Padding.all(10),
+                    width=me.viewport_size().width - 10,
+                    height=me.viewport_size().height - 50,
                     display="flex",
+                    justify_self="center",
                     justify_content="center",
                     align_items="center",
-                ))
-                with me.box(style=me.Style(
-                        # background="white",
-                        width=500,
-                        height=100,
+                    flex_direction="column",
+                    flex_grow=1,
+            )
+            ):
+                with me.box(style=me.Style(margin=me.Margin.all(0),
+                                           background="black",
+                                           width=500,
+                                           height=550,
+                                           display="flex",
+                                           justify_self="center",
+                                           justify_content="center",
+                                           align_self="center",
+                                           align_items="center",
+                                           border_radius=50,
+                                           box_shadow="4px 4px 4px grey",
+                                           flex_direction="column",
+                                           gap=5,
+                                           )
+                            ):
+                    me.icon("person", style=me.Style(
+                        margin=me.Margin.symmetric(vertical=65),
+                        color="black",
+                        background="white",
+                        border_radius=25,
+                        width=40,
+                        height=40,
                         display="flex",
-                        justify_self="center",
                         justify_content="center",
                         align_items="center",
-                        flex_direction="row",
-                        gap=30,
-                        border_radius=50,
-                )
-                ):
-                    # TO-DO:FETCH USERNAME FROM DATABASE
+                    ))
+                    with me.box(style=me.Style(
+                            # background="white",
+                            width=500,
+                            height=100,
+                            display="flex",
+                            justify_self="center",
+                            justify_content="center",
+                            align_items="center",
+                            flex_direction="row",
+                            gap=30,
+                            border_radius=50,
+                    )
+                    ):
+                        # TO-DO:FETCH USERNAME FROM DATABASE
 
-                    me.markdown("Name : ", style=me.Style(
-                        font_family="Arial",
-                        color="white",
-                    ))
-                    me.text(f"{global_state.user_name}", style=me.Style(
-                        font_size=20,
-                        color="white",
-                        font_family="monospace",
-                    ))
+                        me.markdown("Name : ", style=me.Style(
+                            font_family="Arial",
+                            color="white",
+                        ))
+                        me.text(f"{global_state.user_name}", style=me.Style(
+                            font_size=20,
+                            color="white",
+                            font_family="monospace",
+                        ))
 
-                with me.box(style=me.Style(
-                        # background="white",
-                        width=500,
-                        height=100,
-                        display="flex",
-                        justify_self="center",
-                        justify_content="center",
-                        align_items="center",
-                        flex_direction="row",
-                        gap=30,
-                        border_radius=50,
-                )
-                ):
-                    me.markdown("Email ID : ", style=me.Style(
-                        color="white",
-                        font_family="Arial",
-                    ))
-                    me.text(f"{global_state.user_email}", style=me.Style(
-                        font_size=20,
-                        font_family="monospace",
-                        color="white",
-                    ))
+                    with me.box(style=me.Style(
+                            # background="white",
+                            width=500,
+                            height=100,
+                            display="flex",
+                            justify_self="center",
+                            justify_content="center",
+                            align_items="center",
+                            flex_direction="row",
+                            gap=30,
+                            border_radius=50,
+                    )
+                    ):
+                        me.markdown("Email ID : ", style=me.Style(
+                            color="white",
+                            font_family="Arial",
+                        ))
+                        me.text(f"{global_state.user_email}", style=me.Style(
+                            font_size=20,
+                            font_family="monospace",
+                            color="white",
+                        ))
 
 
 
