62ddc2fa94a1a330d0a240195519e87a6edaa2b4
Harinithya <harinithyaveerla@gmail.com>
2024-09-19 22:14:05 +0530

    Update logout_page.py

--- app/logout_page.py
+++ app/logout_page.py
@@ -1,11 +1,74 @@
 from app import global_state
-from app.views import logout_click
+from services.views import logout_click
 import mesop as me
-from components.popupbox_page import popup_box
+
 
 def logout():
     if not global_state.logged_status:
-        popup_box(actions=["You haven't logged in.", "Login", "/login", "Back to Home", "/"])
-
+        # Background box styling
+        with me.box(style=me.Style(
+                background="content-box radial-gradient(#0B0C10, #1F2833)",
+                width=me.viewport_size().width,
+                height=me.viewport_size().height,
+                display="flex",
+                justify_content="center",
+                align_items="center")
+        ):
+            # Confirm Logout box and styling
+            with me.box(style=me.Style(
+                    background="black",
+                    display="flex",
+                    width=500,
+                    height=200,
+                    justify_content="center",
+                    align_items="center",
+                    flex_direction="column",
+                    border_radius=20,
+                    box_shadow="4px 4px 4px grey",
+                    gap=20, )
+            ):
+                me.markdown("You haven't logged in.", style=me.Style(color="white", font_size=15))
+                with me.box(style=me.Style(
+                        # background="yellow",
+                        display="flex",
+                        flex_direction="row",
+                        gap=20)
+                ):
+                    me.button("Login", type="raised", on_click=lambda event: me.navigate("/login"),
+                              style=me.Style(background="radial-gradient(#0B0C10, #1F2833)", color="white"))
+                    me.button("Back to Home", type="raised", on_click=lambda event: me.navigate("/"),
+                              style=me.Style(background="radial-gradient(#0B0C10, #1F2833)", color="white"))
     else:
-        popup_box(actions=["Are you sure you want to Logout?", "Yes", "logout", "No", "/"])
+        # Background box styling
+        with me.box(style=me.Style(
+                background="content-box radial-gradient(#0B0C10, #1F2833)",
+                width=me.viewport_size().width,
+                height=me.viewport_size().height,
+                display="flex",
+                justify_content="center",
+                align_items="center")
+        ):
+            # Confirm Logout box and styling
+            with me.box(style=me.Style(
+                    background="black",
+                    display="flex",
+                    width=500,
+                    height=200,
+                    justify_content="center",
+                    align_items="center",
+                    flex_direction="column",
+                    border_radius=20,
+                    box_shadow="4px 4px 4px grey",
+                    gap=20, )
+            ):
+                me.markdown("Are you sure you want to Logout?", style=me.Style(color="white", font_size=15))
+                with me.box(style=me.Style(
+                        # background="yellow",
+                        display="flex",
+                        flex_direction="row",
+                        gap=20)
+                ):
+                    me.button("Yes", type="raised", on_click=logout_click,
+                              style=me.Style(background="radial-gradient(#0B0C10, #1F2833)", color="white"))
+                    me.button("No", type="raised", on_click=lambda event: me.navigate("/"),
+                              style=me.Style(background="radial-gradient(#0B0C10, #1F2833)", color="white"))
